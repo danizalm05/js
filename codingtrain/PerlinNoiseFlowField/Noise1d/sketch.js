fafb43db15aeb12ad74ed0616fe54915aa9124db
Richm <ric5hme45@yahoo.com>
2020-03-12 20:19:24 +0200

    perlin1

--- codingtrain/PerlinNoiseFlowField/Noise1d/sketch.js
+++ codingtrain/PerlinNoiseFlowField/Noise1d/sketch.js
@@ -3,8 +3,8 @@
 // file:///home/user/Desktop/d/js/codingtrain/PerlinNoiseFlowField/Noise1d/index.html
 // https://www.youtube.com/watch?v=y7sgcFhk6ZM&list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM&index=5
 
-var xoff1 = 0,xoff2 = 1000, yoff = 0;
-var xincrement = 0.01;
+var start = 0;
+var inc = 0.01;
 
 function setup() {
   createCanvas(400,400);
@@ -14,16 +14,29 @@ function setup() {
 
 function draw() {
   background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  var xoff = start;
+   for (var x=0;x<width;x++){
+	stroke(255);
+	 y = noise(xoff) * height;
+	//y = sin(xoff) * height;
+	vertex(x,y); 
+    xoff += inc;	
+   }
+  endShape();
+  start += inc;
+  //noLoop();
+  
    
-  fill(200);
    
-   
-  var x  = map(noise(xoff1),0,1,0,width);
-  var y  = map(noise(xoff2),0,1,0,height);
+  //var x  = map(noise(xoff1),0,1,0,width);
+  //var y  = map(noise(xoff2),0,1,0,height);
  
-  xoff1 += 0.01;
-  xoff2 += 0.01;
- ellipse(x ,y, 64, 64);
+ // xoff1 += 0.01;
+ //xoff2 += 0.01;
+ //ellipse(x ,y, 64, 64);
   
 
   
