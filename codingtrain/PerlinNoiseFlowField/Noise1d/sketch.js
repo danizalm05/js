777e8de177d64070d5e312a18aabff0f516dd344
danizalm05 <danizalm05@gmail.com>
2020-03-13 19:36:09 +0200

    perlin 2d

--- codingtrain/PerlinNoiseFlowField/Noise1d/sketch.js
+++ codingtrain/PerlinNoiseFlowField/Noise1d/sketch.js
@@ -8,36 +8,22 @@ var inc = 0.01;
 
 function setup() {
   createCanvas(400,400);
-  
-  
-}
-
+ }
 function draw() {
   background(51);
   stroke(255);
   noFill();
   beginShape();
   var xoff = start;
+  //console.log(xoff);
    for (var x=0;x<width;x++){
-	stroke(255);
-	 y = noise(xoff) * height;
-	//y = sin(xoff) * height;
-	vertex(x,y); 
-    xoff += inc;	
+	     stroke(255);
+	     y = noise(xoff) * height; 	//y = sin(xoff) * height;
+	     vertex(x,y);
+       xoff += inc;
    }
   endShape();
   start += inc;
+  print
   //noLoop();
-  
-   
-   
-  //var x  = map(noise(xoff1),0,1,0,width);
-  //var y  = map(noise(xoff2),0,1,0,height);
- 
- // xoff1 += 0.01;
- //xoff2 += 0.01;
- //ellipse(x ,y, 64, 64);
-  
-
-  
-}
+ }
